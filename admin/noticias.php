bcc65720e1048051800a33909132a9151a953b53
Thiagowski <thiagopereira3@hotmail.com>
2024-11-27 14:21:01 -0300

    ajustes na exibição das noticias

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require "../includes/cabecalho-admin.php";
 require "../includes/funcoes-noticias.php";
 
@@ -9,61 +9,60 @@ $tipoUsuario = $_SESSION['tipo'];
 $listaDeNoticias = lerNoticias($conexao, $idUsuario, $tipoUsuario);
 ?>
 
-<pre><?=var_dump( $listaDeNoticias)?></pre>
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Notícias <span class="badge bg-dark"><?=count($listaDeNoticias)?> </span>
+			Notícias <span class="badge bg-dark"><?= count($listaDeNoticias) ?> </span>
 		</h2>
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="noticia-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir nova notícia</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir nova notícia</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
 					<tr>
-                        <th>Título</th>
-                        <th>Data</th>
-                        <th>Autor</th>
+						<th>Título</th>
+						<th>Data</th>
+						<th>Autor</th>
 						<th class="text-center">Operações</th>
 					</tr>
 				</thead>
 
 				<tbody>
-
+					<?php foreach ($listaDeNoticias as $noticia) { ?>
 					<tr>
-                        <td> Título da notícia... </td>
-                        <td> 21/12/2112 21:12 </td>
-                        <td> Autor da notícia... </td>
+						<td> <?=$noticia['titulo'] ?> </td>
+						<td> <?=formataData($noticia['data']) ?> </td>
+						<td> <?=$noticia['nome'] ?> </td>
 						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
+							<a class="btn btn-warning"
+								href="noticia-atualiza.php">
+								<i class="bi bi-pencil"></i> Atualizar
 							</a>
-						
-							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
+
+							<a class="btn btn-danger excluir"
+								href="noticia-exclui.php">
+								<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
 					</tr>
-
-				</tbody>                
+					<?php } ?>
+				
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require "../includes/rodape-admin.php";
 ?>
-
