6f729e5757d4ab766099ac2b982aebfc3326c00a
Thiagowski <thiagopereira3@hotmail.com>
2024-11-28 14:30:21 -0300

    include fotos

--- includes/funcoes-noticias.php
+++ includes/funcoes-noticias.php
@@ -71,20 +71,28 @@ WHERE id = $idNoticia";
 }
 
 // Usada em admin/noticia-exclui.php
-function excluirNoticia($conexao, $idNoticia,$idUsuario, $tipoUsuario) {
+function excluirNoticia($conexao, $idNoticia, $idUsuario, $tipoUsuario)
+{
     if ($tipoUsuario === 'admin') {
         $sql = "DELETE FROM noticias
         WHERE id = $idNoticia";
-} else {
-    $sql = "DELETE FROM noticias
+    } else {
+        $sql = "DELETE FROM noticias
     WHERE id = $idNoticia
     AND usuario_id = $idUsuario";
-}
+    }
     executarQuery($conexao, $sql);
 }
 
 
+function lerTodasNoticias($conexao){
+    $sql = "SELECT id, imagem, titulo, resumo FROM noticias
+    ORDER BY data DESC";
 
+    $resultado = executarQuery($conexao, $sql);
+
+    return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}
 
 
 /* *********** */
